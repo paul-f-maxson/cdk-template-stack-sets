e4356ac8e1ce1960f17841424448da69a99d5a19
paul-f-maxson <paul.f.maxson@gmail.com>
2023-05-10 20:39:06 -0400

    slight improvements to stack sets

--- packages/pipeline/lib/IndependentStackSet.ts
+++ packages/pipeline/lib/IndependentStackSet.ts
@@ -5,27 +5,44 @@ import {
 } from "cdk-stacksets";
 import { CfnStackSet } from "aws-cdk-lib";
 
-export type Props<
-  TParameters extends { [parameterKey: string]: string }
+declare type IndependentStackSetProps<
+  TParameters extends Record<string, string> = never
 > = {
+  /**
+   * @summary A callback that adds arbitrary resources to the Stack Set stack
+   * @param stack The stack to which resources will be added
+   */
   withApp: (stack: cdk.Stack) => void;
+  /**
+   * @summary CFN Parameter values that will be passed to stack instances. Overriden by parameters specified at the target level.
+   */
   defaultParameters: TParameters;
+  /**
+   * @summary Environment where stack instances will be located
+   * @description Instances will be created in all regions given for all accounts given.
+   */
   targets: {
     accounts: [string];
+    /**
+     * @summary An array of aws regions for the environments where the Stack Set will be deployed
+     */
     regions: [string];
-    parameters?: TParameters;
+    /**
+     * @summary CFN Parameter values that will be passed to stack instances, overriding defaultParameters
+     */
+    parameters?: Partial<TParameters>;
   }[];
 };
 
 export function withIndependentStackSet<
-  TParameters extends { [parameterKey: string]: string }
+  TParameters extends Record<string, string> = never
 >(
   cdkScope: cdk.Stack,
   {
     withApp,
     defaultParameters,
     targets,
-  }: Props<TParameters>
+  }: IndependentStackSetProps<TParameters>
 ) {
   const appStackSetStack = new StackSetStack(
     cdkScope,
@@ -35,7 +52,10 @@ export function withIndependentStackSet<
   withApp(appStackSetStack);
 
   new CfnStackSet(cdkScope, "StackSet", {
-    stackSetName: "AppStackSet",
+    stackSetName: `${cdk.Names.uniqueResourceName(
+      cdkScope,
+      { separator: "-", maxLength: 60 }
+    )}-AppStackSet`,
     templateUrl: StackSetTemplate.fromStackSetStack(
       appStackSetStack
     ).templateUrl,
@@ -65,5 +85,5 @@ export function withIndependentStackSet<
     ),
   });
 
-  return { appStackSetStack };
+  return {};
 }
--- packages/pipeline/lib/OrganizationsStackSet.ts
+++ packages/pipeline/lib/OrganizationsStackSet.ts
@@ -5,28 +5,48 @@ import {
 } from "cdk-stacksets";
 import { CfnStackSet } from "aws-cdk-lib";
 
-export type Props<
-  TParameters extends { [parameterKey: string]: string }
+declare type OrganizationsStackSetProps<
+  TParameters extends Record<string, string> = never
 > = {
+  /**
+   * @summary A callback that adds arbitrary resources to the Stack Set stack
+   * @param stack The stack to which resources will be added
+   */
   withApp: (stack: cdk.Stack) => void;
+  /**
+   * @summary CFN Parameter values that will be passed to stack instances. Overriden by parameters specified at the target level.
+   */
   defaultParameters: TParameters;
+  /**
+   * @summary Environment where stack instances will be located
+   * @description Instances will be created in all regions given for all accounts given, so long as those accounts are in the specified organization.
+   */
   targets: {
     organizationalUnitId: string;
+    /**
+     * @summary An array of aws account IDs for the environments where the Stack Set will be deployed.
+     */
     accounts: [string];
+    /**
+     * @summary An array of aws regions for the environments where the Stack Set will be deployed
+     */
     regions: [string];
-    parameters?: TParameters;
+    /**
+     * @summary CFN Parameter values that will be passed to stack instances, overriding defaultParameters
+     */
+    parameters?: Partial<TParameters>;
   }[];
 };
 
 export function withOrganizationsStackSet<
-  TParameters extends { [parameterKey: string]: string }
+  T extends Record<string, string> = never
 >(
   cdkScope: cdk.Stack,
   {
     withApp,
     defaultParameters,
     targets,
-  }: Props<TParameters>
+  }: OrganizationsStackSetProps<T>
 ) {
   const appStackSetStack = new StackSetStack(
     cdkScope,
@@ -36,7 +56,10 @@ export function withOrganizationsStackSet<
   withApp(appStackSetStack);
 
   new CfnStackSet(cdkScope, "StackSet", {
-    stackSetName: `${cdkScope.stackName}-AppStackSet`,
+    stackSetName: `${cdk.Names.uniqueResourceName(
+      cdkScope,
+      { separator: "-", maxLength: 60 }
+    )}-AppStackSet`,
     templateUrl: StackSetTemplate.fromStackSetStack(
       appStackSetStack
     ).templateUrl,
@@ -75,4 +98,6 @@ export function withOrganizationsStackSet<
       })
     ),
   });
+
+  return {};
 }
